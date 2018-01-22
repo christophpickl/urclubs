94aca6f5695fc45bb0dfac3e1ca3ca5fd248967e
Christoph Pickl <christoph.pickl@erstegroup.com>
2018-01-22 11:49:31 +0100

    introduce guice

--- src/main/kotlin/com/github/christophpickl/urclubs/App.kt
+++ src/main/kotlin/com/github/christophpickl/urclubs/App.kt
@@ -1,11 +1,13 @@
 package com.github.christophpickl.urclubs
 
 import ch.qos.logback.classic.Level
-import com.github.christophpickl.kpotpourri.common.collection.prettyPrint
 import com.github.christophpickl.kpotpourri.logback4k.Logback4k
 import com.github.christophpickl.urclubs.backend.MyClubsApi
+import com.google.inject.AbstractModule
+import com.google.inject.Guice
+import javax.inject.Inject
 
-object UrClubs {
+object AppStarter {
 
     init {
         configureLogging()
@@ -13,11 +15,9 @@ object UrClubs {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val myclubs = MyClubsApi(parseCredentials(args))
-
-        myclubs.login()
-//        myclubs.partners().prettyPrint()
-        myclubs.activities().prettyPrint()
+        val credentials = parseCredentials(args)
+        val guice = Guice.createInjector(MainModule(credentials))
+        guice.getInstance(UrClubsApp::class.java).start()
     }
 
     private fun parseCredentials(args: Array<String>): Credentials {
@@ -38,6 +38,25 @@ object UrClubs {
                 pattern = "[%-5level] %logger{60} - %msg%n"
             }
         }
+    }
+
+}
+
+class UrClubsApp @Inject constructor(
+        private val myclubs: MyClubsApi
+) {
+   fun start() {
+       myclubs.login()
+       println(myclubs.loggedUser())
+//        myclubs.partners().prettyPrint()
+//       myclubs.activities().prettyPrint()
+    }
+}
+
+class MainModule(private val credentials: Credentials) : AbstractModule() {
+    override fun configure() {
+        bind(MyClubsApi::class.java).toInstance(MyClubsApi(credentials))
+        bind(UrClubsApp::class.java)
     }
 
 }
