5a4c77eb1f7fd18ee25dd3c8866909f90b364b98
christophpickl <christoph.pickl@gmail.com>
2018-04-30 14:46:47 +0200

    introducing system tests

---
+++ src/test/kotlin/com/github/christophpickl/urclubs/myclubs/MyClubsHttpApiSystemTest.kt
@@ -0,0 +1,12 @@
+package com.github.christophpickl.urclubs.myclubs
+
+import org.testng.annotations.Test
+
+@Test(groups = ["system"])
+class MyClubsHttpApiSystemTest {
+
+    fun `foo bar`() {
+        // FIXME implement system test
+    }
+
+}
