e4ba258fe429d737192f009a8e811cf047b48ed9
christophpickl <christoph.pickl@gmail.com>
2018-09-11 20:26:55 +0200

    release done

--- src/test/kotlin/non_test/release.kt
+++ src/test/kotlin/non_test/release.kt
@@ -1,41 +1,67 @@
 package non_test
 
 import ch.qos.logback.classic.Level
+import com.github.christophpickl.kpotpourri.github.AssetUpload
+import com.github.christophpickl.kpotpourri.github.CreateReleaseRequest
 import com.github.christophpickl.kpotpourri.github.RepositoryConfig
 import com.github.christophpickl.kpotpourri.github.buildGithub4k
 import com.github.christophpickl.kpotpourri.github.detectGithubPass
 import com.github.christophpickl.kpotpourri.logback4k.Logback4k
-import com.github.christophpickl.kpotpourri.release4k.release4k
 import java.io.File
+import java.io.FileReader
+import java.util.Properties
 
 fun main(args: Array<String>) {
     configureLogging()
 
-    release4k {
-        println("Urclubs Release - START")
-        println()
+    println("Urclubs Release - START")
+    println()
 
-        execute("bash", "bin/release.sh", File("."))
+//    ProcessExecuterImpl().execute("bash", "bin/release.sh", File("."))
 
-        // -Dgithub.pass=...
-        val githubApi = buildGithub4k(RepositoryConfig(
-            repositoryOwner = "christophpickl",
-            repositoryName = "urclubs",
-            username = "christoph.pickl@gmail.com",
-            password = detectGithubPass()
-        ))
+    val version = readVersion()
+    uploadGithub(version)
 
-//        github4k.uploadReleaseAsset(AssetUpload(
-//            releaseId = 444,
-//            bytes = file.readBytes(),
-//            fileName = file.name,
-//            contentType = "application/x-apple-diskimage"
-//        ))
+    println()
+    println("Urclubs Release - END")
 
-        println()
-        println("Urclubs Release - END")
+}
+
+private fun readVersion(): String {
+    val props = Properties()
+    props.load(FileReader(File("version.properties")))
+    return props.getProperty("version")
+}
+
+private fun uploadGithub(version: String) {
+    // -Dgithub.pass=...
+    println("Connecting to GitHub ...")
+    val githubApi = buildGithub4k(RepositoryConfig(
+        repositoryOwner = "christophpickl",
+        repositoryName = "urclubs",
+        username = "christoph.pickl@gmail.com",
+        password = detectGithubPass()
+    ))
+
+    println("Creating new release ...")
+    val release = githubApi.createNewRelease(CreateReleaseRequest(
+        tag_name = version,
+        name = "Release $version",
+        body = "Simple auto release of UrClubs version $version"
+    ))
+
+    val file = File("build/distributions/UrClubs-$version.dmg")
+    if (!file.exists()) {
+        throw Exception("Expected release artifact does not exist at: ${file.canonicalPath}")
     }
 
+    println("Uploading disk image located at ${file.canonicalPath} ...")
+    githubApi.uploadReleaseAsset(AssetUpload(
+        releaseId = release.id,
+        bytes = file.readBytes(),
+        fileName = file.name,
+        contentType = "application/x-apple-diskimage"
+    ))
 }
 
 
